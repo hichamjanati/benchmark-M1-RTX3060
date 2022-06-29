3891073d3d9a118f17b6693430519eba7f835503
Hicham Janati <hicham.janati100@gmail.com>
2022-06-29 11:51:49 +0200

    add m1 data

---
+++ plot_benchmark.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+with open("scores_")
+scores_m1
---
+++ run_benchmark.py
@@ -0,0 +1,42 @@
+import torch
+import pickle
+import numpy as np
+from time import time
+
+
+def f(n, device):
+    t = time()
+    A = torch.randn(n, n).to(device)
+    B = torch.randn(n, n).to(device)
+    C = (A @ B).sum()
+    return time() - t
+
+devices = ["cpu"]
+
+if torch.cuda.is_available():
+    torch.cuda.init()
+    devices.append("cuda:0")
+    device_names = ["AMD Ryzen 7 5700", "RTX 3060"]
+    filename = 'victus'
+else:
+    device_names = ["M1"]
+    filename = 'macbook'
+
+
+if __name__ == "__main__":
+    n_runs = 10
+    ns = [5, 20, 100, 500, 1000, 2000, 3000]
+    scores = dict()
+
+    for device, device_name in zip(devices, device_names):
+        times = np.empty((n_runs, len(ns)))
+        for jj, n in enumerate(ns):
+            for run in range(n_runs):
+                times[run, jj] = f(n, device)
+        scores[device_name] = times
+    
+    with open("scores-%s.pkl" % filename, 'wb') as ff:
+        pickle.dump(scores, ff)
+    
+
+
